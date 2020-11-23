5b21ede1fbdd1a1fa7b6a93721a0655e2c1fb76b
Destin Martin <destin@destins-mbp.gateway.sasktel.net>
2020-11-22 18:38:35 -0600

    reiterating quordinate system test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 
+let lastMove;
+
 const PORT = process.env.PORT || 3000
 
 const app = express()
@@ -34,71 +36,21 @@ function handleStart(request, response) {
 }
 
 function handleEnd(request, response) {
-  var gameData = request.body
-
+  var gameData = request.body;
   console.log('END')
   response.status(200).send('ok')
 }
 
-let lastMove;
+
 function handleMove(request, response) {
-  var gameData = request.body
+  var gameData = request.body;
   console.log("lastMove: " +lastMove);
 
   var possibleMoves = ['up', 'down', 'left', 'right']
 
-  function preventCollision(gameData) {
-    body = gameData.body;
-    console.log(gameData.you.body);
-  }
 
 
-  function stayOnBoard(gameData){
-    function removeMove(x) {
-      let index = possibleMoves.indexOf(x);
-      if (index > -1){
-        possibleMoves.splice(index, 1);
-      }
-    }
-    //TODO: First we want to check the size of the board, and make sure we stay on the board.
-    //if head = on left, bottom, top or rigth edge of the board, then
-        // var  possibleMoves = !the way to die
-
-        // If the last move was up, we can't go down. if the last move was left, we cant go right. 
-        let height = gameData.board.height;
-        let width = gameData.board.width;
-        let quox = gameData.you.head.x;  //horizontal
-        let quoy = gameData.you.head.y; //vertical 
-
-        console.log("width: " +width + ", x: "+ quox + ", y: "+ quoy);
-
-        const leftSide = 0;
-        const rightSide = width - 1;
-        const top = height - 1;
-        const bottom = 0;
-        
-
-        /* 
-        This method won't work, as it's splicing by location in the array.  The location is changing before the second removal is activated. 
-        */
-        if (quox === rightSide || lastMove === "left"){ //horizontal avoidance
-          console.log('cant go right')
-          removeMove("right");
-        }
-        if (quox === leftSide || lastMove === "right"){
-          console.log('cant go left')
-          removeMove("left");
-        }
-        if (quoy === top || lastMove === "down"){
-          console.log('cant go up')
-          removeMove("up");
-        }
-        if (quoy === bottom || lastMove === "up"){
-          console.log('cant go down')
-          removeMove("down");
-        }        
-}
-
+  
   stayOnBoard(gameData); // WORKS!!
   preventCollision(gameData); // TODO: define the quardinates of the body, and keep 1 block from the snake
 
@@ -121,8 +73,85 @@ function handleMove(request, response) {
 
 
 }
+function preventCollision(gameData) {
+
+}
+
+const snakeFactory = (name) =>{
+  let enemySnakes = gameData.board.snakes;
+  let location;
+  
+  const checkLocation = ((name) => {
+    let selSnake;
+    if(name === "you"){
+       selsnake = gameData.you;
+    }
+    else{
+      selSnake = enemySnakes[name];
+    }
+    location = {
+      'head': {
+        'x':selSnake.head.x,
+        'y':selSnake.head.y
+      },
+      'tail': { 
+        'x': selSnake.tail.x,
+        'y': selSnake.tail.y
+        },
+      'body':SelSnake.body,
+    }
+    console.log("snake initialized" + name)
+  })();
+
+  return {name, location, length}// return the snake
+}
 
 
+function stayOnBoard(gameData){
+  function removeMove(x) {
+    let index = possibleMoves.indexOf(x);
+    if (index > -1){
+      possibleMoves.splice(index, 1);
+    }
+  }
+    // First we want to check the size of the board, and make sure we stay on the board.
+       //if head = on left, bottom, top or rigth edge of the board, then
+      // var  possibleMoves = !the way to die
+
+      // If the last move was up, we can't go down. if the last move was left, we cant go right. 
+      let height = gameData.board.height;
+      let width = gameData.board.width;
+      let quox = gameData.you.head.x;  //horizontal
+      let quoy = gameData.you.head.y; //vertical 
+      let quoxTail = gameData.you.tail.x;
+      let quoyTail = gameData.you.tail.y;
+      console.log("width: " +width + ", x: "+ quox + ", y: "+ quoy);
+
+      const leftSide = 0;
+      const rightSide = width - 1;
+      const top = height - 1;
+      const bottom = 0;
+      
+      /* 
+      This method won't work, as it's splicing by location in the array.  The location is changing before the second removal is activated. 
+      */
+      if (quox === rightSide || lastMove === "left"){ //horizontal avoidance
+        console.log('cant go right')
+        removeMove("right");
+      }
+      if (quox === leftSide || lastMove === "right"){
+        console.log('cant go left')
+        removeMove("left");
+      }
+      if (quoy === top || lastMove === "down"){
+        console.log('cant go up')
+        removeMove("up");
+      }
+      if (quoy === bottom || lastMove === "up"){
+        console.log('cant go down')
+        removeMove("down");
+      }        
+}
 
 // {
 //   game: {
@@ -148,3 +177,29 @@ function handleMove(request, response) {
 //     shout: ''
 //   }
 // }
+
+
+// [ { x: 5, y: 6 },
+//   { x: 6, y: 6 },
+//   { x: 6, y: 5 },
+//   { x: 6, y: 4 },
+//   { x: 6, y: 3 },
+//   { x: 6, y: 2 },
+//   { x: 6, y: 1 },
+//   { x: 6, y: 0 },
+//   { x: 5, y: 0 },
+//   { x: 4, y: 0 },
+//   { x: 3, y: 0 },
+//   { x: 2, y: 0 },
+//   { x: 1, y: 0 },
+//   { x: 0, y: 0 },
+//   { x: 0, y: 1 },
+//   { x: 0, y: 2 },
+//   { x: 0, y: 3 },
+//   { x: 0, y: 4 },
+//   { x: 0, y: 5 },
+//   { x: 0, y: 6 },
+//   { x: 1, y: 6 },
+//   { x: 2, y: 6 },
+//   { x: 3, y: 6 },
+//   { x: 4, y: 6 } ]
