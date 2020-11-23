55efd4f472f2ef7d7991e4180614495fb382f6eb
Destin Martin <destin@destins-mbp.gateway.sasktel.net>
2020-11-23 15:42:37 -0600

    testing hazard detection with body of snake

--- index.js
+++ index.js
@@ -1,228 +1,216 @@
-const bodyParser = require('body-parser')
-const express = require('express')
+const bodyParser = require("body-parser");
+const express = require("express");
 
 let lastMove;
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
-app.get('/', handleIndex)
-app.post('/start', handleStart)
-app.post('/move', handleMove)
-app.post('/end', handleEnd)
+app.get("/", handleIndex);
+app.post("/start", handleStart);
+app.post("/move", handleMove);
+app.post("/end", handleEnd);
 
-app.listen(PORT, () => console.log(`Battlesnake Server listening at http://127.0.0.1:${PORT}`))
-
+app.listen(PORT, () =>
+  console.log(`Battlesnake Server listening at http://127.0.0.1:${PORT}`)
+);
 
 function handleIndex(request, response) {
   var battlesnakeInfo = {
-    apiversion: '1',
-    author: 'DestinTech',
-    color: '#6600cc',
-    head: 'beluga',
-    tail: 'bolt'
-  }
-  response.status(200).json(battlesnakeInfo)
+    apiversion: "1",
+    author: "DestinTech",
+    color: "#6600cc",
+    head: "beluga",
+    tail: "bolt",
+  };
+  response.status(200).json(battlesnakeInfo);
 }
 
 function handleStart(request, response) {
-  var gameData = request.body
-  console.log(gameData.board.width)
-  
-  console.log('START')
-  response.status(200).send('ok')
+  var gameData = request.body;
+  console.log(gameData.board.width);
+
+  console.log("START");
+  response.status(200).send("ok");
 }
 
 function handleEnd(request, response) {
   var gameData = request.body;
-  console.log('END')
-  response.status(200).send('ok')
+  console.log("END");
+  response.status(200).send("ok");
 }
 
-let counter=0;
+let counter = 0;
 
 function handleMove(request, response) {
   var gameData = request.body;
-  var possibleMoves = ['up', 'down', 'left', 'right'];
+  var possibleMoves = ["up", "down", "left", "right"];
   let move;
-  let me = snakeFactory('you', gameData);
+  let me = snakeFactory("you", gameData);
   //TODO: make a class for game board that holds me, enemies, and the game board variables along with any other properties
   console.log("\n New Move... Thinking...");
-  console.log("lastMove: " +lastMove);
+  console.log("lastMove: " + lastMove);
 
   stayOnBoard(gameData, possibleMoves); // WORKS!!
-  if (counter < 3){
+
+  // randomize move every 3 moves to allow collision detection testing
+  if (counter < 3) {
     move = stayOnTrack(possibleMoves);
     counter++;
-  }
-  else{
-    move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)] //otherwise, random move that's avaialable.
+  } else {
+    move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]; //otherwise, random move that's avaialable.
     counter = 0;
   }
-
 
   preventCollision(gameData, move, me); // TODO: define the quardinates of the body, and keep 1 block from the snake
 
   console.log(possibleMoves);
-  console.log('MOVE: ' + move + "\n")
+  console.log("MOVE: " + move + "\n");
   lastMove = move;
   response.status(200).send({
-    move: move
+    move: move,
   });
 }
 
-function preventCollision(gameData,move, me) {
+function preventCollision(gameData, move, me) {
   //here we will check what space the "move" we want to make will occupy.
   me = me;
 
-  
-  
-  function lookAhead(){
-let oracle={
-  head:{
-  'x':0,
-  'y':0
-  }
-}
-    if (move === "down"){
-      oracle.head.y = me.location.head.y -1; 
+  function lookAhead() {
+    let oracle = {
+      head: {
+        x: 0,
+        y: 0,
+      },
+    };
+    if (move === "down") {
+      oracle.head.y = me.location.head.y - 1;
       oracle.head.x = me.location.head.x;
-    }
-    else if(move === "up"){
-      oracle.head.y = me.location.head.y +1;
+    } else if (move === "up") {
+      oracle.head.y = me.location.head.y + 1;
       oracle.head.x = me.location.head.x;
-
-    }
-    else if(move === "left"){
-      oracle.head.x = me.location.head.x -1;
+    } else if (move === "left") {
+      oracle.head.x = me.location.head.x - 1;
       oracle.head.y = me.location.head.y;
-
-    }
-    else if(move === "right"){
-      oracle.head.x = me.location.head.x +1;
+    } else if (move === "right") {
+      oracle.head.x = me.location.head.x + 1;
       oracle.head.y = me.location.head.y;
-    }
-    else{
-      console.log('error');
+    } else {
+      console.log("error");
     }
     return oracle;
   }
 
-
-  function checkHazards(me, nextMove) {
-    let snake = me.location; 
-    let hazards = [    //make a list of hazard locations
+  function checkHazards(me, nextMove, possibleMoves) {
+    let snake = me.location;
+    let hazards = [
+      //make a list of hazard locations
       snake.body,
-    ]
+    ];
     //check for quordinate == the locations to our expected next move
-  console.log("Hazards: ");
-  for (let hazard in hazards){
-    haz = hazards[hazard];
+    console.log("Hazards: ");
+    for (let hazard in hazards) {
+      haz = hazards[hazard];
+      console.log({ haz });
+      if (nextMove === hazards[hazard]) {
+        removeMove(nextMove, possibleMoves);
+      }
+    }
+  
 
-    console.log({nextMove, haz }) ;
-  }
-
-  }
-
-
-
-  nextMove = lookAhead();//get the quordinates of the chosen next move
-  checkHazards(me, nextMove);//check for hazards on the next quordinate,
+  nextMove = lookAhead(); //get the quordinates of the chosen next move
+  checkHazards(me, nextMove, possibleMoves); //check for hazards on the next quordinate,
   //TODO:check for enemies close to the next quordinate, to see if it's a head. If it's a head, compare size. if size is bigger, move towards quordinate.
   console.log(nextMove);
   return nextMove;
 }
 
-
-
-
-const snakeFactory = (name, gameData) =>{
+const snakeFactory = (name, gameData) => {
   //let enemySnakes = gameData.board.snakes;
   //console.log(enemySnakes);
   let location;
   let length;
   let selSnake;
   name = name;
-  const checkLocation = (() => {
-    if(name === "you"){
-        selSnake = gameData.you;
-    }
-    else{
+  const checkLocation = () => {
+    if (name === "you") {
+      selSnake = gameData.you;
+    } else {
       //selSnake = enemySnakes[name];
     }
     location = {
-      'head': {
-        'x':selSnake.head.x,
-        'y':selSnake.head.y
+      head: {
+        x: selSnake.head.x,
+        y: selSnake.head.y,
       },
-      //'tail': { 
-       // 'x': selSnake.tail.x,
-       // 'y': selSnake.tail.y
-       // },
-      'body': selSnake.body,
-    }
-    console.log("snake initialized: " + name)
-  });
-
+      //'tail': {
+      // 'x': selSnake.tail.x,
+      // 'y': selSnake.tail.y
+      // },
+      body: selSnake.body,
+    };
+    console.log("snake initialized: " + name);
+  };
 
   checkLocation();
-  return {name, location,length}// return the snake
+  return { name, location, length }; // return the snake
+};
+
+function removeMove(move, possibleMoves) {
+  let index = possibleMoves.indexOf(move);
+  if (index > -1) {
+    possibleMoves.splice(index, 1);
+  }
 }
 
+function stayOnBoard(gameData, possibleMoves) {
+  // First we want to check the size of the board, and make sure we stay on the board.
+  //if head = on left, bottom, top or rigth edge of the board, then
+  // var  possibleMoves = !the way to die
 
-function stayOnBoard(gameData, possibleMoves){
-  function removeMove(x) {
-    let index = possibleMoves.indexOf(x);
-    if (index > -1){
-      possibleMoves.splice(index, 1);
-    }
-  }
-    // First we want to check the size of the board, and make sure we stay on the board.
-       //if head = on left, bottom, top or rigth edge of the board, then
-      // var  possibleMoves = !the way to die
+  // If the last move was up, we can't go down. if the last move was left, we cant go right.
+  let height = gameData.board.height;
+  let width = gameData.board.width;
+  let quox = gameData.you.head.x; //horizontal
+  let quoy = gameData.you.head.y; //vertical
 
-      // If the last move was up, we can't go down. if the last move was left, we cant go right. 
-      let height = gameData.board.height;
-      let width = gameData.board.width;
-      let quox = gameData.you.head.x;  //horizontal
-      let quoy = gameData.you.head.y; //vertical 
+  const leftSide = 0;
+  const rightSide = width - 1;
+  const top = height - 1;
+  const bottom = 0;
 
-      const leftSide = 0;
-      const rightSide = width - 1;
-      const top = height - 1;
-      const bottom = 0;
-   
-      /* 
+  /* 
       This method won't work, as it's splicing by location in the array.  The location is changing before the second removal is activated. 
       */
-      if (quox === rightSide || lastMove === "left"){ //horizontal avoidance
-        console.log('cant go right')
-        removeMove("right");
-      }
-      if (quox === leftSide || lastMove === "right"){
-        console.log('cant go left')
-        removeMove("left");
-      }
-      if (quoy === top || lastMove === "down"){
-        console.log('cant go up')
-        removeMove("up");
-      }
-      if (quoy === bottom || lastMove === "up"){
-        console.log('cant go down')
-        removeMove("down");
-      }        
+  if (quox === rightSide || lastMove === "left") {
+    //horizontal avoidance
+    console.log("cant go right");
+    removeMove("right", possibleMoves);
+  }
+  if (quox === leftSide || lastMove === "right") {
+    console.log("cant go left");
+    removeMove("left", possibleMoves);
+  }
+  if (quoy === top || lastMove === "down") {
+    console.log("cant go up");
+    removeMove("up", possibleMoves);
+  }
+  if (quoy === bottom || lastMove === "up") {
+    console.log("cant go down");
+    removeMove("down", possibleMoves);
+  }
 }
 
-function stayOnTrack(possibleMoves){
+function stayOnTrack(possibleMoves) {
   let index = possibleMoves.indexOf(lastMove); //find if the last move is in the list
-    if (index > -1){ // if it is in the list
-      return lastMove;  // do what we did last if it's an option.
-    }
-    else{
-      return possibleMoves[Math.floor(Math.random() * possibleMoves.length)] //otherwise, random move that's avaialable.
-    }
+  if (index > -1) {
+    // if it is in the list
+    return lastMove; // do what we did last if it's an option.
+  } else {
+    return possibleMoves[Math.floor(Math.random() * possibleMoves.length)]; //otherwise, random move that's avaialable.
+  }
 }
 
 // {
@@ -249,10 +237,6 @@ function stayOnTrack(possibleMoves){
 //     shout: ''
 //   }
 // }
-
-
-
-
 
 // [ { x: 5, y: 6 },
 //   { x: 6, y: 6 },
